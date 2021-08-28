63eb7eb3add48784207374b27f39d0e4e49d5b2a
Edson Mito <89589245+mitoedson@users.noreply.github.com>
2021-08-28 15:09:37 -0300

    Create ex01_03.c

---
+++ ex01_03.c
@@ -0,0 +1,27 @@
+/* Exercício básico para cálculo de área do cícrulo, ao qual o usuário irá introduzir um valor: o comprimento do raio do círculo, e receberá um resultado (área do círculo).
+
+Considere raio = 3.14 
+
+SO: Linux Mint
+Compilador GCC
+*/
+
+#include <stdio.h> // biblioteca  de entrada e saida de dados
+#include <math.h> // biblioteca matemática
+
+int main() // Função principal
+{
+    // Início do programa
+    float raio;
+    printf("\n\t\t==========  Aula 01 - Ex.03  =========");
+    printf("\n\t\t===== Calculo da area do circulo =====\n\n");
+
+    printf("Insira o comprimento do raio do circulo (em m): ");
+    scanf("%f",&raio);
+
+    printf("\nA area do circulo sera %.2f ", 3.14*raio*raio);
+    printf("metros quadrados (raio = 3.14 m).\n\n");
+  
+    return 0;
+    // Fim do programa
+}
