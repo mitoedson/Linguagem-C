87f71ac95f481ed2f789409d05e4026cbe2c2b41
Edson Mito <89589245+mitoedson@users.noreply.github.com>
2021-08-29 13:14:23 -0300

    Update ex01_02.c

--- ex01_02.c
+++ ex01_02.c
@@ -1,1 +1,30 @@
+/*
+Codificado em 28/08/2021, por Edson Mito.
+SO: Linux Mint
+Compilador GCC
 
+Descrição: Exercício básico para cálculo de área do retângulo, ao qual o usuário irá introduzir dois valores: o comprimento da altura e da largura do retângulo), e receberá um resultado (área do retângulo). 
+*/
+
+#include <stdio.h> // biblioteca  de entrada e saida de dados
+#include <math.h> // biblioteca matemática
+
+int main() // Função principal
+{
+    // Início do programa
+    float altura,largura;
+    printf("\n\t\t==========  Aula 01 - Ex.02  =========");
+    printf("\n\t\t==== Calculo da area do retangulo ====\n\n");
+
+    printf("Insira o comprimento da altura do retangulo (em m): ");
+    scanf("%f",&altura);
+
+    printf("\nInsira o comprimento da largura do retangulo (em m): ");
+    scanf("%f",&largura);
+
+    printf("\nA area do retangulo sera %.2f ", altura*largura);
+    printf("metros quadrados.\n\n");
+  
+    return 0;
+    // Fim do programa
+}
