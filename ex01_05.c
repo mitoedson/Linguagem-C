331056c15f5843948127a970fceabe529214c682
Edson Mito <89589245+mitoedson@users.noreply.github.com>
2021-08-29 13:20:04 -0300

    Update ex01_05.c

--- ex01_05.c
+++ ex01_05.c
@@ -1,7 +1,10 @@
-/* Exercício básico que solicita ao usuário inserir dois valores numéricos inteiros, e o programa verifica a diferença entre eles.
-
+/*
+Arquivo: ex01_05.c
+Codificado em 28/08/2021, por Edson Mito.
 SO: Linux Mint
 Compilador GCC
+
+Descrição: Exercício básico que solicita ao usuário inserir dois valores numéricos inteiros, e o programa verifica a diferença entre eles.
 */
 
 #include <stdio.h> // biblioteca  de entrada e saida de dados
